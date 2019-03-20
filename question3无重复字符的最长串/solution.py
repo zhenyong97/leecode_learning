b24faf9f20b5922bf240ee94441c1d80b20e9b42
callmehero <506141737@qq.com>
2019-03-20 22:36:53 +0800

    无重复字符串

---
+++ question3无重复字符的最长串/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        st = {}
+        i, ans = 0, 0
+        for j in range(len(s)):
+            if s[j] in st:
+                i = max(st[s[j]], i)
+            ans = max(ans, j - i + 1)
+            st[s[j]] = j + 1
+        return ans;
+		
